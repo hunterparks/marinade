8c25ac8002bc44db453f309612e29f43d803e61c
hunterparks <parksh@msoe.edu>
2017-11-29 15:47:30 -0600

    Changed app component selector to match prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'marinade-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
