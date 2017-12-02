81ada053b491ded045d419e6350fd6c1d7755320
hunterparks <parksh@msoe.edu>
2017-12-02 15:57:27 -0600

    Installed tslint-eslint-rules, added more tslint rules, added lint step to ci config, and fixed some linting issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
+  bootstrap: [AppComponent],
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,5 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+  // tslint:disable-next-line:no-console
+  .catch((err: any) => console.log(err));
