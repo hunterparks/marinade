24239b46232ed7f8a4a20ee25fb59a1cda7cf99e
Alex Roberts <robertsaj@msoe.edu>
2017-12-05 17:36:42 -0600

    Added websocket communication proof of concept

--- electron.js
+++ electron.js
@@ -1,5 +1,6 @@
 // ./main.js
 const {app, BrowserWindow} = require('electron');
+const path = require('path');
 
 let win = null;
 
@@ -26,10 +27,36 @@ app.on('activate', () => {
   if (win === null) {
     createWindow();
   }
-})
+});
 
 app.on('window-all-closed', function () {
-  if (process.platform != 'darwin') {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
+
+let pyProc = null;
+let pyPort = null;
+
+const selectPort = () => {
+  pyPort = 4242;
+  return pyPort
+};
+
+const createPyProc = () => {
+  let port = '' + selectPort();
+  let script = path.join(__dirname, 'backend', 'src', 'api.py');
+  pyProc = require('child_process').spawn('python', [script, port]);
+  if (pyProc !== null) {
+    console.log('child process success')
+  }
+};
+
+const exitPyProc = () => {
+  pyProc.kill();
+  pyProc = null;
+  pyPort = null;
+};
+
+app.on('ready', createPyProc);
+app.on('will-quit', exitPyProc);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import { WebsocketService } from '../services/websocket.service';
+
 @Component({
   selector: 'marinade-root',
   styleUrls: ['./app.component.css'],
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
-  public title: string = 'app';
+  public formula: string = '1 + 1';
+  public result: string = '';
+
+  constructor(private serial: WebsocketService) {
+    this.serial.messageSubject.subscribe((result: string) => {
+      this.result = result;
+    });
+    this.serial.connect();
+  }
+
+  public onChange(): void {
+    /* tslint:disable-next-line: no-console */ // Todo: Remove this output
+    console.log(this.formula);
+    this.serial.write(this.formula);
+  }
 }
