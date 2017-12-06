5d12d247142ca8e7ce78419b5a6d093df096c122
Skuse <skusel@ad.msoe.edu>
2017-12-06 15:19:17 -0600

    Cleaned up resiter file class

--- simulator_prototype/components/register_file.py
+++ simulator_prototype/components/register_file.py
@@ -5,18 +5,9 @@ from components.logic_input import LogicInput
 from components.abstract.sequential import Sequential
 from enum import Enum
 
-class Register_Edge_Type(Enum):
-    LATCH_RISING_EDGE = 0
-    LATCH_FALLING_EDGE = 1
-    LATCH_BOTH_EDGE = 2
+class Register_File(Sequential):
 
-class Register_Reset_Type(Enum):
-    RESET_ACTIVE_LOW = 0
-    RESET_ACTIVE_HIGH = 1
-
-class Register(Sequential):
-
-    def __init__(self, name, clock, reset, regwr, wd3, a1, a2, a3, rd1, rd2, size, default_state = 0, edge_type = Register_Edge_Type.LATCH_RISING_EDGE, reset_type = Register_Reset_Type.RESET_ACTIVE_LOW):
+    def __init__(self, name, clock, reset, regwr, wd3, a1, a2, a3, rd1, rd2, size, default_state = 0, edge_type = 'RISING_EDGE', reset_type = 'ACTIVE_LOW'):
 
         self._name = name
         self._clock = clock
@@ -34,40 +25,26 @@ class Register(Sequential):
         self._edge_type = edge_type
         self._reset_type = reset_type
 
+    #TODO
     def on_rising_edge(self):
-        if self._edge_type == Register_Edge_Type.LATCH_RISING_EDGE or self._edge_type == Register_Edge_Type.LATCH_BOTH_EDGE:
-            self._q = self._d
+        pass
 
+    #TODO
     def on_falling_edge(self):
-        if self._edge_type == Register_Edge_Type.LATCH_FALLING_EDGE or self._edge_type == Register_Edge_Type.LATCH_BOTH_EDGE:
-            self._q = self._d
+        pass
 
+    #TODO
     def on_reset(self):
-        self._q = self._default_state
+        pass
 
+    #TODO
     def inspect(self):
-        return {'name' : self._name, 'type' : 'register', 'size' : self._size, 'state' : self._q}
+        pass
 
-    #TODO write this function so that user can modify internal contents of register
+    #TODO
     def modify(self,data):
         pass
 
+    #TODO 
     def run(self):
-        # receive input from in bud
-        self._d = self._in_bus.read()
-
-        #check for clock change
-        if self._clock.read() == 1 and self._prev_clock_state == 0:
-            self.on_rising_edge()
-        elif self._clock.read() == 0 and self._prev_clock_state == 1:
-            self.on_falling_edge()
-        self._prev_clock_state = self._clock.read()
-
-        #check for reset event
-        if self._reset_type == Register_Reset_Type.RESET_ACTIVE_LOW and self._reset.read() == 0:
-            self.on_reset()
-        elif self._reset_type == Register_Reset_Type.RESET_ACTIVE_HIGH and self._reset.read() == 1:
-            self.on_reset()
-
-        # assert output for timestep
-        self._out_bus.write(self._q)
+        pass
