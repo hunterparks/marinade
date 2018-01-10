d81a82e5091a419c34518d0dcc7cba3730999c20
curthenrichs <henrichsc@msoe.edu>
2018-01-10 15:56:51 -0600

    electron working

--- electron.js
+++ electron.js
@@ -1,36 +1,25 @@
 // ./main.js
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow} = require('electron');
 
 let win = null;
 
-function createDefaultWindow() {
+app.on('ready', function () {
+
   // Initialize the window to our specified dimensions
-  win = new BrowserWindow({ width: 1000, height: 600 });
+  win = new BrowserWindow({width: 1000, height: 600});
+
+  // Specify entry point
+  win.loadURL('http://localhost:4200');
+
   // Show dev tools
   // Remove this line before distributing
   win.webContents.openDevTools();
+
   // Remove window once app is closed
   win.on('closed', function () {
     win = null;
   });
-  let urlSource = process.argv[2];
-  // Specify entry point
-  if (urlSource === 'dynamic') {
-    win.loadURL('http://localhost:4200');
-  } else {
-    win.loadURL(`file://${__dirname}/dist/index.html`);
-  }
-  return win;
-}
 
-app.on('ready', function() {
-  createDefaultWindow();
-});
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
 });
 
 app.on('activate', () => {
@@ -38,3 +27,9 @@ app.on('activate', () => {
     createWindow();
   }
 })
+
+app.on('window-all-closed', function () {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
