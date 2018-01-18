cc03bf0fc11f8d97f9820f0afe9a558bda495909
Alex Roberts <robertsaj@msoe.edu>
2018-01-18 12:47:15 -0600

    Added customizable configuration files for sentry, added frontend/backend context tags to errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import * as Raven from 'raven-js';
-import { WebsocketService } from '../services/websocket.service';
 import { AppComponent } from './app.component';
+import { WebsocketService } from './services/websocket.service';
+import { SentrySettings } from './settings/sentry/local.sentry.settings';
 
-Raven
-  .config('http://3d4bca0e8aa8465bae6fd29934f5ea0f@192.168.64.2:9000/1')
-  .install();
+Raven.config(SentrySettings.getURL()).install();
+Raven.setTagsContext({
+  'Aspect': 'Frontend',
+  'Language': 'TypeScript',
+});
 
 export class RavenErrorHandler implements ErrorHandler {
   public handleError(err: any): void {
