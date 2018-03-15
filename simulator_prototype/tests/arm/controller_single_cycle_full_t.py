49585a9a3201007ae6f02727a045625ef28a7d79
curthenrichs <henrichsc@msoe.edu>
2018-03-15 18:17:50 -0500

    full controller unit test

--- simulator_prototype/tests/arm/controller_single_cycle_full_t.py
+++ simulator_prototype/tests/arm/controller_single_cycle_full_t.py
@@ -12,27 +12,146 @@ from components.core.bus import Bus
 class ControllerSingleCycle_t(unittest.TestCase):
     """
     Test ControllerSingleCycle's constructor, run and hook functionality
+
+    Note: Run tested through simulation
     """
 
     def test_constructor(self):
         "tests 4 bad constructors - not all possible constructors tested"
-        raise NotImplementedError
+        instruction = Bus(32)
+        c = Bus(1)
+        v = Bus(1)
+        n = Bus(1)
+        z = Bus(1)
+        pcwr = Bus(1, 0)
+        regsa = Bus(1, 0)
+        regsb = Bus(1, 0)
+        regdst = Bus(2, 0)
+        regwrs = Bus(2, 0)
+        wdbs = Bus(1, 0)
+        regwr = Bus(1, 0)
+        exts = Bus(2, 0)
+        alusrcb = Bus(1, 0)
+        alus = Bus(4, 0)
+        aluflagwr = Bus(1, 0)
+        shop = Bus(2, 0)
+        shctrl = Bus(2, 0)
+        accen = Bus(1, 0)
+        memwr = Bus(1, 0)
+        memty = Bus(2, 0)
+        regsrc = Bus(1, 0)
+        pcsrc = Bus(2, 0)
 
-    def test_run(self):
-        "tests the signle cycle processors run method"
-        raise NotImplementedError
+        scc = ControllerSingleCycle(instruction,c,v,n,z,pcsrc,pcwr,regsa,
+                                    regdst,regsb,regwrs,regwr,exts,alusrcb,
+                                    alus,shop,shctrl,accen,aluflagwr,memty,
+                                    memwr,regsrc,wdbs)
 
     def test_inspect(self):
         "tests the single cycle processors inspect method"
-        raise NotImplementedError
+        instruction = Bus(32)
+        c = Bus(1)
+        v = Bus(1)
+        n = Bus(1)
+        z = Bus(1)
+        pcwr = Bus(1, 0)
+        regsa = Bus(1, 0)
+        regsb = Bus(1, 0)
+        regdst = Bus(2, 0)
+        regwrs = Bus(2, 0)
+        wdbs = Bus(1, 0)
+        regwr = Bus(1, 0)
+        exts = Bus(2, 0)
+        alusrcb = Bus(1, 0)
+        alus = Bus(4, 0)
+        aluflagwr = Bus(1, 0)
+        shop = Bus(2, 0)
+        shctrl = Bus(2, 0)
+        accen = Bus(1, 0)
+        memwr = Bus(1, 0)
+        memty = Bus(2, 0)
+        regsrc = Bus(1, 0)
+        pcsrc = Bus(2, 0)
+
+        scc = ControllerSingleCycle(instruction,c,v,n,z,pcsrc,pcwr,regsa,
+                                    regdst,regsb,regwrs,regwr,exts,alusrcb,
+                                    alus,shop,shctrl,accen,aluflagwr,memty,
+                                    memwr,regsrc,wdbs)
+
+        ins = scc.inspect()
+        self.assertTrue(ins['type'] == 'sc-controller')
+        self.assertTrue(ins['state'] is None)
 
     def test_modify(self):
         "tests the single cycle processor modify method"
-        raise NotImplementedError
+        instruction = Bus(32)
+        c = Bus(1)
+        v = Bus(1)
+        n = Bus(1)
+        z = Bus(1)
+        pcwr = Bus(1, 0)
+        regsa = Bus(1, 0)
+        regsb = Bus(1, 0)
+        regdst = Bus(2, 0)
+        regwrs = Bus(2, 0)
+        wdbs = Bus(1, 0)
+        regwr = Bus(1, 0)
+        exts = Bus(2, 0)
+        alusrcb = Bus(1, 0)
+        alus = Bus(4, 0)
+        aluflagwr = Bus(1, 0)
+        shop = Bus(2, 0)
+        shctrl = Bus(2, 0)
+        accen = Bus(1, 0)
+        memwr = Bus(1, 0)
+        memty = Bus(2, 0)
+        regsrc = Bus(1, 0)
+        pcsrc = Bus(2, 0)
+
+        scc = ControllerSingleCycle(instruction,c,v,n,z,pcsrc,pcwr,regsa,
+                                    regdst,regsb,regwrs,regwr,exts,alusrcb,
+                                    alus,shop,shctrl,accen,aluflagwr,memty,
+                                    memwr,regsrc,wdbs)
+
+        mod = scc.modify(None)
+        self.assertTrue('error' in mod)  # modify is not implemented for controller
+
+        mod = scc.modify({'state': 0})
+        self.assertTrue('error' in mod)  # modify is not implemented for controller
 
     def test_clear(self):
         "tests the single cycle processor clear method"
-        raise NotImplementedError
+        instruction = Bus(32)
+        c = Bus(1)
+        v = Bus(1)
+        n = Bus(1)
+        z = Bus(1)
+        pcwr = Bus(1, 0)
+        regsa = Bus(1, 0)
+        regsb = Bus(1, 0)
+        regdst = Bus(2, 0)
+        regwrs = Bus(2, 0)
+        wdbs = Bus(1, 0)
+        regwr = Bus(1, 0)
+        exts = Bus(2, 0)
+        alusrcb = Bus(1, 0)
+        alus = Bus(4, 0)
+        aluflagwr = Bus(1, 0)
+        shop = Bus(2, 0)
+        shctrl = Bus(2, 0)
+        accen = Bus(1, 0)
+        memwr = Bus(1, 0)
+        memty = Bus(2, 0)
+        regsrc = Bus(1, 0)
+        pcsrc = Bus(2, 0)
+
+        scc = ControllerSingleCycle(instruction,c,v,n,z,pcsrc,pcwr,regsa,
+                                    regdst,regsb,regwrs,regwr,exts,alusrcb,
+                                    alus,shop,shctrl,accen,aluflagwr,memty,
+                                    memwr,regsrc,wdbs)
+
+        mod = scc.clear()
+        self.assertTrue('error' in mod)  # modify is not implemented for controller
 
 
 if __name__ == '__main__':
