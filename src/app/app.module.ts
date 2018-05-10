6c233489dadc677d4249dfbb213f4a54a07a20cd
hunterparks <parksh@msoe.edu>
2018-05-09 21:41:19 -0500

    Basic save functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,19 +39,21 @@ import { ResponseService } from './services/simulator/response/response.service'
 import { RequestService } from './services/simulator/request/request.service';
 import { WebsocketService } from './services/simulator/websocket/websocket.service';
 import { TooltipService } from './services/tooltip/tooltip.service';
-import { SentrySettings } from './settings/sentry/local.sentry.settings';
+import { IpcService } from './services/ipc/ipc.service';
+import { EditorFileService } from './services/editor/file/editor-file.service';
+// import { SentrySettings } from './settings/sentry/local.sentry.settings';
 
-Raven.config(SentrySettings.getURL()).install();
-Raven.setTagsContext({
-  'Aspect': 'Frontend',
-  'Language': 'TypeScript',
-});
+// Raven.config(SentrySettings.getURL()).install();
+// Raven.setTagsContext({
+//   'Aspect': 'Frontend',
+//   'Language': 'TypeScript',
+// });
 
-export class RavenErrorHandler implements ErrorHandler {
-  public handleError(err: any): void {
-    Raven.captureException(err);
-  }
-}
+// export class RavenErrorHandler implements ErrorHandler {
+//   public handleError(err: any): void {
+//     Raven.captureException(err);
+//   }
+// }
 
 @NgModule({
   bootstrap: [AppComponent],
@@ -99,7 +101,9 @@ export class RavenErrorHandler implements ErrorHandler {
     TooltipService,
     RequestService,
     WebsocketService,
-    { provide: ErrorHandler, useClass: RavenErrorHandler }
+    IpcService,
+    EditorFileService,
+    // { provide: ErrorHandler, useClass: RavenErrorHandler }
   ]
 })
 export class AppModule { }
