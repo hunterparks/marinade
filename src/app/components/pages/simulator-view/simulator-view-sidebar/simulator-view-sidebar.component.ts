51a85ef33c2916a2e45d45de119c4ad45707295b
Alex Roberts <robertsaj@msoe.edu>
2018-05-24 19:55:03 -0500

    Fixed event handler for the sidebar "step" button

--- src/app/components/pages/simulator-view/simulator-view-sidebar/simulator-view-sidebar.component.ts
+++ src/app/components/pages/simulator-view/simulator-view-sidebar/simulator-view-sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { SVGPath } from '@models/simulator/svg/path.class';
 import { ArchitectureService } from '@services/simulator/architecture/architecture.service';
+import { RequestService } from '@services/simulator/request/request.service';
 
 @Component({
   selector: 'marinade-simulator-view-sidebar',
@@ -8,7 +10,7 @@ import { ArchitectureService } from '@services/simulator/architecture/architectu
 })
 export class SimulatorViewSidebarComponent {
 
-  private readonly SIDEBAR_CONFIG: any = [
+  public readonly SIDEBAR_CONFIG: any = [
     { callingID: 'load',   icon: 'folder-open-o',  title: 'Load'   },
     { callingID: 'reset',  icon: 'undo',           title: 'Reset'  },
     { callingID: 'unload', icon: 'times-circle-o', title: 'Unload' },
@@ -16,12 +18,18 @@ export class SimulatorViewSidebarComponent {
     { callingID: 'step',   icon: 'step-forward',   title: 'Step'   },
   ];
 
-  constructor(private architectureService: ArchitectureService) { }
+  constructor(private architectureService: ArchitectureService, private requestService: RequestService) { }
 
   public sidebarClickHandler(source: string): void {
     switch (source) {
       case 'load': this.architectureService.load(); break;
       case 'unload': this.architectureService.unload(); break;
+      case 'step':
+        this.requestService.step('logic');
+        this.architectureService.architecture.getValue().path.forEach((path: SVGPath) => {
+          path.inspect();
+        });
+        break;
       default: break;
     }
   }
